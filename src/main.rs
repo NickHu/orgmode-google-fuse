953e3536569f34d8f5e25aeab26934bfe6c386ce
Nick Hu <me@nickhu.co.uk>
2025-06-17 14:44:52 +0100

    gracefully unmount after shutdown

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,32 @@ async fn main() -> std::io::Result<()> {
         .collect()
         .await;
 
-    let () = fuser::mount2(
+    let _handle = fuser::spawn_mount2(
         OrgFS::new(calendars, tasklists),
         &args.mount,
         &[MountOption::FSName("orgmode-google-fuse".to_string())],
     )?;
+
+    // handle SIGINT and SIGTERM to unmount gracefully
+    let int = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install SIGINT handler");
+    };
+    let term = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM handler")
+            .recv()
+            .await;
+    };
+    tokio::select! {
+        _ = int => {
+            tracing::info!("Received SIGINT, unmounting…");
+        }
+        _ = term => {
+            tracing::info!("Received SIGTERM, unmounting…");
+        }
+    }
+
     Ok(())
 }
